8c0fab5e49f993fbb2925e70b2c1cf838e48a1b4
unknown <sagarmikeylevi@gmail.com>
2024-01-12 15:23:13 +0530

    Starting project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-xl text-orange-400 font-bold uppercase">
+        Jay Shree Raam
+      </div>
+    </>
+  );
+}
+
+export default App;
